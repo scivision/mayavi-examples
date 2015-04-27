99c970af805bcc4a39f8d1b3a5e85b6723758890
scienceopen <scienceopen@users.noreply.github.com>
2015-04-27 12:13:20 -0400

    demo

--- volumetrans.py
+++ volumetrans.py
@@ -14,7 +14,7 @@ requires Python 2 due to mayavi and vtk
 def gsim(z0=200, h0=50, n0=1e12, zlim=(100,500),dz=2.,
                                  xlim=(-50,50),   dx=1.,
                                  ylim=(-100,100),  dy=1.,
-                                 xsc=1/50, ysc=1/30):
+                                 xsc=1/100, ysc=1/150):
 
     # keep the axes in x,y,z order instead of z,x,y.
     # must be mgrid, NOT ogrid
@@ -22,8 +22,8 @@ def gsim(z0=200, h0=50, n0=1e12, zlim=(100,500),dz=2.,
                      ylim[0]:ylim[1]+dy:dy,
                      zlim[0]:zlim[1]+dz:dz]
     ne = chapman(z0,h0,n0,z)
-    ne = modu(x,1/50,ne)
-    ne = modu(y,1/30,ne)
+    ne = modu(x,xsc,ne)
+    ne = modu(y,ysc,ne)
 
     return ne,x,y,z
 
@@ -35,14 +35,16 @@ def modu(p,mp,ne):
     return ne  *(0.5*np.cos(2*np.pi*mp*p)+1)
 
 def plotsim(s, x,y,z):
+    p1090 = np.percentile(s,(10,90)) #for axes limit
 #%% transparent volume example
     """
     http://docs.enthought.com/mayavi/mayavi/auto/mlab_pipeline_other_functions.html
     can manipulate opacity and combine with mutable slices
     """
+
     scf = mlab.pipeline.scalar_field(x,y,z,s)
     fig1 = mlab.gcf()
-    makevol(scf,fig1)
+    makevol(scf,p1090,fig1)
     figlbl(fig1)
 #%% mutable slice
     fig2=mlab.figure()
@@ -51,9 +53,9 @@ def plotsim(s, x,y,z):
     figlbl(fig2)
     mlab.show()
 
-def makevol(scf,figh=None):
+def makevol(scf,p1090,figh=None):
     """    transparent, pretty but hard to quantify    """
-    mlab.pipeline.volume(scf,figure=figh)
+    mlab.pipeline.volume(scf,figure=figh,vmin=p1090[0],vmax=p1090[1])
 
 def makeslice(scf,figh=None):
     """
